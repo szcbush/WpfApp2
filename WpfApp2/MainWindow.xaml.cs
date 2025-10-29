6cbf2ac378ccac6c657149d671b957e15e642474
zhichengshi <zhichengshi@LAPTOP-7D4Q070U>
2025-10-29 10:42:44 +0800

    添加项目文件。

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace LanguageAssessmentSystem
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            if (username == "admin" && password == "111")
+            {
+                MessageBox.Show("登录成功！");
+            }
+            else
+            {
+                MessageBox.Show("用户名或密码错误，请重试。");
+            }
+        }
+    }
+}
