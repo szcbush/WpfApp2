6e8f9dbcde6822e7427dc40c868c33fc180950f9
zhichengshi <2277869418@qq.com>
2025-11-05 16:40:44 +0800

    修改功能实现

--- WpfApp2/page/MyUser.xaml.cs
+++ WpfApp2/page/MyUser.xaml.cs
@@ -1,31 +1,16 @@
 ﻿using Microsoft.Data.Sqlite;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WpfApp2.page.entity;
 namespace WpfApp2.page
 {
-    /// <summary>
-    /// MyUser.xaml 的交互逻辑
-    /// </summary>
     public partial class MyUser : Page
     {
         private ObservableCollection<UserInfo> _users;
-        private string _statusMessage;
-
 
         private const string ConnectionString = "Data Source=users.db";
         public MyUser()
@@ -50,7 +35,7 @@ namespace WpfApp2.page
             try
             {
                 // 这里替换为实际的数据库查询代码
-                ObservableCollection < UserInfo > u1 = new ObservableCollection<UserInfo>();
+                ObservableCollection<UserInfo> u1 = new ObservableCollection<UserInfo>();
                 using (var conn = new SqliteConnection(ConnectionString))
                 {
                     conn.Open();
@@ -67,7 +52,7 @@ namespace WpfApp2.page
                             Birthday = DateTime.Parse(reader["birthday"].ToString()),
                             PhoneNumber = reader["phone_number"].ToString()
                         });
-                        }
+                    }
                     Users = u1;
                 }
 
@@ -78,38 +63,160 @@ namespace WpfApp2.page
             }
         }
 
-        // 查看报告按钮点击事件
-        private void ViewReport_Click(object sender, RoutedEventArgs e)
+        
+        private void DeleteUser_Click(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
             if (button != null)
             {
                 string userId = button.Tag.ToString();
-                // 这里实现查看报告的逻辑
-                MessageBox.Show($"查看用户 {userId} 的报告");
+                // 
             }
         }
 
-        // 修改用户按钮点击事件
+        // 修改用户按钮
         private void EditUser_Click(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
             if (button != null)
             {
                 string userId = button.Tag.ToString();
-                // 这里实现修改用户的逻辑  
-                MessageBox.Show($"修改用户 {userId} 的信息");
+
+                UserInfo user = new UserInfo();
+                using (var conn = new SqliteConnection(ConnectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT id,username,gender,birthday,phone_number from users where id = @userId";
+                    using var cmd = new SqliteCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@userId", userId);
+                    using var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        user = new UserInfo
+                        {
+                            UserId = reader["id"].ToString(),
+                            Name = reader["username"].ToString(),
+                            Gender = reader["gender"].ToString(),
+                            Birthday = DateTime.Parse(reader["birthday"].ToString()),
+                            PhoneNumber = reader["phone_number"].ToString()
+                        };
+                    }
+                }
+                EditUserIdText.Text = user.UserId;
+                EditNameTextBox.Text = user.Name;
+                EditBirthdayDatePicker.SelectedDate = user.Birthday;
+                EditPhoneTextBox.Text = user.PhoneNumber;
+                foreach (ComboBoxItem item in EditGenderComboBox.Items)
+                {
+                    if (item.Content.ToString() == user.Gender)
+                    {
+                        EditGenderComboBox.SelectedItem = item;
+                        break;
+                    }
+                }
+
+                EditModalOverlay.Visibility = Visibility.Visible;
             }
         }
 
-        // 添加用户按钮点击事件
-        private void AddUser_Click(object sender, RoutedEventArgs e)
+        private void CloseEditModal_Click(object sender, RoutedEventArgs e)
         {
-            // 这里实现添加用户的逻辑
-            MessageBox.Show("打开添加用户窗口");
+            closeModal();
         }
 
-        // INotifyPropertyChanged 实现
+        private void closeModal() {
+            EditModalOverlay.Visibility = Visibility.Collapsed;
+            EditUserIdText.Text = string.Empty;
+            EditNameTextBox.Text = string.Empty;
+            EditGenderComboBox.SelectedIndex = 0;
+            EditBirthdayDatePicker.SelectedDate = null;
+            EditPhoneTextBox.Text = string.Empty;
+        }
+
+        // 修改
+        // 修改
+        private void SaveModal_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(EditNameTextBox.Text))
+            {
+                MessageBox.Show("请输入姓名", "提示", MessageBoxButton.OK, MessageBoxImage.Warning);
+                EditNameTextBox.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(EditPhoneTextBox.Text))
+            {
+                MessageBox.Show("请输入电话号码", "提示", MessageBoxButton.OK, MessageBoxImage.Warning);
+                EditPhoneTextBox.Focus();
+                return;
+            }
+
+            UserInfo user = new UserInfo();
+            user.UserId = EditUserIdText.Text.Trim();
+            user.Name = EditNameTextBox.Text.Trim();
+            user.Gender = (EditGenderComboBox.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "男";
+            user.Birthday = EditBirthdayDatePicker.SelectedDate ?? DateTime.Now;
+            user.PhoneNumber = EditPhoneTextBox.Text.Trim();
+
+            // 数据库修改用户代码
+            try
+            {
+                using (var conn = new SqliteConnection(ConnectionString))
+                {
+                    conn.Open();
+                    string query = @"UPDATE users 
+                            SET username = @UserName, 
+                                gender = @Gender, 
+                                birthday = @Birthday, 
+                                phone_number = @PhoneNumber 
+                            WHERE id = @UserId";
+
+                    using var cmd = new SqliteCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@UserName", user.Name);
+                    cmd.Parameters.AddWithValue("@Gender", user.Gender);
+                    cmd.Parameters.AddWithValue("@Birthday", user.Birthday.ToString("yyyy-MM-dd"));
+                    cmd.Parameters.AddWithValue("@PhoneNumber", user.PhoneNumber);
+                    cmd.Parameters.AddWithValue("@UserId", user.UserId);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        // 更新本地数据集合
+                        var localUser = Users.FirstOrDefault(u => u.UserId == user.UserId);
+                        if (localUser != null)
+                        {
+                            localUser.Name = user.Name;
+                            localUser.Gender = user.Gender;
+                            localUser.Birthday = user.Birthday;
+                            localUser.PhoneNumber = user.PhoneNumber;
+
+                            // 触发UI更新
+                            OnPropertyChanged(nameof(Users));
+                        }
+
+                        MessageBox.Show("用户信息修改成功！", "成功", MessageBoxButton.OK, MessageBoxImage.Information);
+                        closeModal();
+                    }
+                    else
+                    {
+                        MessageBox.Show("修改失败，未找到对应的用户", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"修改用户信息失败：{ex.Message}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        // 添加用户
+        private void AddUser_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+       
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged(string propertyName)
         {
